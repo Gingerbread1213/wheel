85619599eecf0f02f45330639b37fe98c5740e6d
刘豪杰 <liuhaojie@liuhaojiedeMacBook-Pro-2.local>
2023-04-08 17:49:07 -0700

    4/8

---
+++ sketch_apr8a/sketch_apr8a.ino
@@ -0,0 +1,36 @@
+
+#include <Arduino.h>
+#include <TM1637Display.h>
+
+#define CLK D6
+#define DIO D7
+
+TM1637Display display(CLK, DIO);
+
+
+void setup() {
+
+  display.setBrightness(0x0f);
+
+  pinMode(D8, INPUT);
+  display.showNumberDec(0);
+
+}
+
+void loop() {
+
+  int index;
+
+  while(true){
+
+    if(digitalRead(D8) == HIGH){
+      index++;
+
+    }
+
+    display.showNumberDec(index);
+    Serial.println(index);
+  }
+
+
+}
