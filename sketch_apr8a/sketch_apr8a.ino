974db5cfed1be50c24363d87ff85875e55b9fd03
刘豪杰 <liuhaojie@liuhaojiedeMacBook-Pro-2.local>
2023-04-08 19:53:13 -0700

    change

--- sketch_apr8a/sketch_apr8a.ino
+++ sketch_apr8a/sketch_apr8a.ino
@@ -1,4 +1,3 @@
-//傻逼
 #include <Arduino.h>
 #include <TM1637Display.h>
 
@@ -7,30 +6,27 @@
 
 TM1637Display display(CLK, DIO);
 
+int num = 0; // 在函数外部定义变量
+
 
 void setup() {
-
   display.setBrightness(0x0f);
-
-  pinMode(D8, INPUT);
+  pinMode(D2, INPUT);
+  pinMode(D0, INPUT);
   display.showNumberDec(0);
-
+  num = 0; // 初始化全局变量
 }
 
 void loop() {
-
-  int index;
-
-  while(true){
-
-    if(digitalRead(D8) == HIGH){
-      index++;
-
-    }
-
-    display.showNumberDec(index);
-    Serial.println(index);
+  if(digitalRead(D2) == LOW) {
+    num++;
+    display.showNumberDec(num);
   }
-
-
+  if(digitalRead(D0) == HIGH){
+    setup();
+  }
 }
+
+
+
+
